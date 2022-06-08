4301655f31d2ccccd39efa1592af551a6c093436
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-06-08 13:39:26 -0700

    mobile updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,16 +34,16 @@ const Home: NextPage = () => {
           content="https://www.depth.wiki/meta.png"
         />
       </Head>
-      <main className="flex flex-col items-center justify-center h-screen w-full space-y-3">
+      <main className="flex flex-col items-center justify-center h-screen w-full space-y-3 p-2">
         <img
           className="w-48 h-auto"
           src="https://upload.wikimedia.org/wikipedia/commons/8/80/Wikipedia-logo-v2.svg  "
           alt="Wikipedia logo"
         />
-        <h1 className="text-3xl font-bold font-georgia ">
+        <h1 className="text-3xl font-bold font-georgia text-center">
           The Depths of Wikipedia
         </h1>
-        <p className="text-xl">
+        <p className="lg:text-xl text-center text-base">
           Click the button to explore a random corner of Wikipedia.
         </p>
         <Link href="/api/random" as={`/api/random`} passHref prefetch={true}>
@@ -52,13 +52,13 @@ const Home: NextPage = () => {
           </a>
         </Link>
 
-        <p className="text-sm absolute bottom-0 p-2 w-1/2 flex justify-between">
-          <span>
+        <p className="text-sm absolute bottom-0 p-2 w-full md:w-1/2 flex justify-between">
+          <span className="">
             Created by{" "}
             <a
               href="
           https://twitter.com/eiioth"
-              className="text-blue-500"
+              className="text-blue-500 block md:inline-block"
             >
               @eiioth
             </a>
@@ -68,7 +68,7 @@ const Home: NextPage = () => {
             <a
               href="
           https://twitter.com/depthsofwiki"
-              className="text-blue-500"
+              className="text-blue-500 block md:inline-block"
             >
               @depthsofwiki
             </a>
