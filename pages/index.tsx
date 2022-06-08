214c579362442c916d0cae2db967c673ea34aee1
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-06-08 14:12:41 -0700

    twitter tags cause me pain

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,8 @@ const Home: NextPage = () => {
       <Head>
         <title>The Depths of Wikipedia</title>
         <link rel="icon" href="https://www.depth.wiki/favicon.svg" />
-
         <meta name="title" content="The Depths of Wikipedia" />
         <meta name="description" content="Explore the depths of Wikipedia" />
-
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://depth.wiki/" />
         <meta property="og:title" content="The Depths of Wikipedia" />
@@ -21,19 +19,9 @@ const Home: NextPage = () => {
           content="Explore the depths of Wikipedia"
         />
         <meta property="og:image" content="https://www.depth.wiki/meta.png" />
-
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:site" content="@eiioth" />
-        <meta property="twitter:url" content="https://depth.wiki/" />
-        <meta property="twitter:title" content="The Depths of Wikipedia" />
-        <meta
-          property="twitter:description"
-          content="Explore the depths of Wikipedia"
-        />
-        <meta
-          property="twitter:image"
-          content="https://www.depth.wiki/meta.png"
-        />
+        <meta property="twitter:site" content="Eliot Hertenstein" />
+        <meta name="twitter:widgets:new-embed-design" content="on" />
       </Head>
       <main className="flex flex-col items-center justify-center h-screen w-full space-y-3 p-2">
         <img
